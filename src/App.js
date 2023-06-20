649aa32c59021ff89fdaae4dbfff24db1812831c
Alaa Elghamry <alaa.elghamry7@gmail.com>
2023-06-20 15:23:24 +0300

    get new item

--- src/App.js
+++ src/App.js
@@ -24,21 +24,24 @@ function Logo() {
 }
 function Form() {
 
-const [description , setDescription] = useState('');
+const [description , setDescription] = useState("");
+const [quantity , setQuantity] = useState(0);
+
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+  const newItem ={description,quantity,packed:false , id:Date.now()}
+  console.log(newItem);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit} >
       <h3>What do you need for your ✈ trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
     {Array.from({length :20}, (_,i) => i+1).map(num=>
       <option value={num} key={num}>{num}</option>
     )}
       </select>
-    <input type="text" placeholder="Item..." />
+    <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
     <button>Add</button>
   </form>
   )
